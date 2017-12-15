dc38d6f98163a9858fe03292d562c66287ab81e5
hitoshi <murozono1030@gmail.com>
2017-12-15 21:50:37 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def show
+        @book = Book.find(params[:id])
+  end
+
+  def new
+        @book = Book.new
+        @books = Book.all
+  end
+
+  def edit
+        @book = Book.find(params[:id])
+  end
+
+  def create
+        book = Book.new(book_params)
+        book.save
+        flash[:notice] = 'Book was successfully created.'
+        redirect_to book_path(book.id)
+  end
+
+  def index
+      @books = Book.all
+  end
+
+  def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+        flash[:updated] = 'Book was successfully updated.'
+        redirect_to book_path(@book.id)
+  end
+
+  def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        flash[:destroyed] = 'Book was successfully destroyed.'
+        redirect_to new_book_path
+    end
+
+  private
+      def book_params
+        params.require(:book).permit(:title, :body)
+      end
+end
