805bceba4f8a3b69a35f10d4e7e42140fb36e9e7
hitoshi <murozono1030@gmail.com>
2017-12-24 19:42:25 +0900

    課題提出

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,42 @@
 class UsersController < ApplicationController
-	def edit
-        @user = User.find(params[:id])
-    end
+  before_action :authenticate_user!, except: [:top, :about]
 
-    def show
-     @user = User.find(params[:id])
-     @book = Book.new
-     @books = Book.all
+  def top
+    if user_signed_in?
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+	def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
 	end
+
+	def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+	end
+
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, # ユーザー名は2~20文字
+  validates :name, # ユーザー名は2~20文字、空白・重複は認めない
   			uniqueness: { case_sensitive: :false },
-  			length: { minimum: 2, maximum: 20 }
+  			length: { minimum: 2, maximum: 20 },
+  			presence: true
+
+  validates :introduction, # 自己紹介文の長さは最大50文字
+  			length: { maximum: 50 }
 
   has_many :books  # 投稿と1:Nの関係性
 
-  attachment :profile_image 
+  attachment :profile_image
 
 end
